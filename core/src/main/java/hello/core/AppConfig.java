101757aa47b94b24bb8466540b4c25d4965fd830
Gyuri Kim <kgr3819@gmail.com>
2022-08-02 19:27:02 +0900

    [기본편] 스프링으로 전환하기

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -6,24 +6,30 @@ import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.*;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-public class AppConfig {
+@Configuration //설정정보에 붙이는 어노테이션
+public class AppConfig { //구성정보, 설정정보의 파일이라는 이름
 
     //역할을 세우고 딱 구현이 어떤 것으로 되는지 바로 확인할 수 있음
-
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public MemberRepository memberRepository(){
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(
                 memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
         //return new FixDiscountPolicy();
         return new RateDiscountPolicy();
--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -3,11 +3,21 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
         AppConfig appConfig = new AppConfig();
         MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        //모든 객체를 관리해줌 (Bean 이런것들)
+        //AppConfig 에 있는 환경정보 설정을 가지고 스프링이 @Bean 을 스프링 컨테이너에 집어넣어서 관리해줌
+
+        //AppConfig 에 등록되어있는 [메서드 이름, 타입] 그대로 가져와야함 함.
+        //스프링 컨테이너에서 꺼낼 때는 메서드 이름으로 꺼내면 됨
+       applicationContext.getBean("memberService", MemberService.class);
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
