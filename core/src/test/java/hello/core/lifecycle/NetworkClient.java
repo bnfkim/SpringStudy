34fd831b08070aea0773f4ee1b74c9cedc8072d6
Gyuri Kim <kgr3819@gmail.com>
2022-08-11 16:19:38 +0900

    [기본편] 빈 동록 초기화, 소멸 메서드

--- core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,16 +1,11 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-//        connect();
-//        call("초기화 연결 메시지"); -> after 로 옮김
     }
 
     public void setUrl(String url) {
@@ -31,19 +26,14 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close: " + url);
     }
 
-    @Override
-    //의존관계 주입이 끝나면 호출해주겠다 !
-    //싱글톤 빈이기 때문에 컨테이너가 올라올 때 이미 생성이 되고, 의존관계가 끝나면 그 후 호출됨
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("NetworkClient.afterPropertiesSet");
+    public void init() {
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
-    @Override
-    //종료 후 끝나면 호출
-    public void destroy() throws Exception {
-        System.out.println("NetworkClient.destroy");
+    public void close(){
+        System.out.println("NetworkClient.close");
         disconnect();
     }
 }
