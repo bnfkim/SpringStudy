6c24be62533d8fe4eabdded00fdb312271f0b316
Gyuri Kim <kgr3819@gmail.com>
2022-08-11 16:23:44 +0900

    [기본편] @] @PostConstruct, @PreDestory

--- core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package hello.core.lifecycle;
 
+import javax.annotation.PostConstruct; //다른 컨테이너를 쓴다 하더라도 지원함
+import javax.annotation.PreDestroy;
+
 public class NetworkClient {
 
     private String url;
@@ -26,12 +29,14 @@ public class NetworkClient {
         System.out.println("close: " + url);
     }
 
+    @PostConstruct //스프링에 등록되는 방법이 아님
     public void init() {
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
+    @PreDestroy
     public void close(){
         System.out.println("NetworkClient.close");
         disconnect();
