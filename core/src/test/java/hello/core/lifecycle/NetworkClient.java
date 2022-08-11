4e970a61bb14cfd4bec0d604358d6a1d666a4bc5
Gyuri Kim <kgr3819@gmail.com>
2022-08-11 15:26:07 +0900

    [기본편]  initializingBean, DisposableBean

--- core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,13 +1,16 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
+//        connect();
+//        call("초기화 연결 메시지"); -> after 로 옮김
     }
 
     public void setUrl(String url) {
@@ -20,11 +23,27 @@ public class NetworkClient {
     }
 
     public void call(String message) {
-        System.out.println("call : " + url + "message = " + message);
+        System.out.println("call : " + url + " message = " + message);
     }
 
     //서비스 종료시 호출출
     public void  disconnect() {
         System.out.println("close: " + url);
+    }
+
+    @Override
+    //의존관계 주입이 끝나면 호출해주겠다 !
+    //싱글톤 빈이기 때문에 컨테이너가 올라올 때 이미 생성이 되고, 의존관계가 끝나면 그 후 호출됨
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    //종료 후 끝나면 호출
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 }
