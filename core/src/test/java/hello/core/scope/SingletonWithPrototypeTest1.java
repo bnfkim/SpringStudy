84ecc7bfdca5d4fcb8cf1d273bf8c52a018f1381
Gyuri Kim <kgr3819@gmail.com>
2022-08-13 21:03:37 +0900

    [기본편] 프로토타입 스코프

---
+++ core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -0,0 +1,48 @@
+package hello.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class SingletonWithPrototypeTest1 {
+
+    @Test
+    void prototypeFind() {
+
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        prototypeBean1.addCount();
+        Assertions.assertThat(prototypeBean1.getCount()).isEqualTo(1);
+
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        private int count = 0;
+
+        public void addCount() {
+            count++;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+        @PostConstruct
+        public void init() {
+            //나의 참조 값이 출력됨
+            System.out.println("PrototypeBean.init " + this);
+        }
+
+        @PreDestroy
+        public void destroy() {
+            //나의 참조 값이 출력됨
+            System.out.println("PrototypeBean.destroy " + this);
+        }
+
+    }
+}
