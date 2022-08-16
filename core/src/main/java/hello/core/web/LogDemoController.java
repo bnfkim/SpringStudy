d011e643082aed7cd1e3b60635ab4f161a070f76
Gyuri Kim <kgr3819@gmail.com>
2022-08-16 15:23:47 +0900

    [기본편] scope와 provider

--- core/src/main/java/hello/core/web/LogDemoController.java
+++ core/src/main/java/hello/core/web/LogDemoController.java
@@ -2,6 +2,7 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,12 +14,14 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider; //myLogger을 주입받는게 아니라 찾을 수 있는 디펜더시가 생성됨
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) { //고객 요청 정보를 받을 수 있음
+
         String requestURL = request.getRequestURL().toString(); //어떤 URL로 들어왔는지 알 수 있음
+        MyLogger myLogger = myLoggerProvider.getObject(); //필요한 시점에 myLogger을 주입받음음
         myLogger.setMyLogger(requestURL);
 
         myLogger.log("controller test");
