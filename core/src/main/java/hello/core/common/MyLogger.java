71595b1056c509875411f71363f02c150e3f3591
Gyuri Kim <kgr3819@gmail.com>
2022-08-16 16:22:17 +0900

    [기본편] 스코프와 프록시

--- core/src/main/java/hello/core/common/MyLogger.java
+++ core/src/main/java/hello/core/common/MyLogger.java
@@ -1,6 +1,7 @@
 package hello.core.common;
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -8,13 +9,13 @@ import javax.annotation.PreDestroy;
 import java.util.UUID;
 
 @Component
-@Scope(value = "request")
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS) //가짜 프록시를 만듬
 public class MyLogger {
 
     private String uuid;
     private String requestURL;
 
-    public void setMyLogger(String requestURL) {
+    public void setRequestURL(String requestURL) {
         this.requestURL = requestURL;
     }
 
--- core/src/main/java/hello/core/web/LogDemoController.java
+++ core/src/main/java/hello/core/web/LogDemoController.java
@@ -14,15 +14,18 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider; //myLogger을 주입받는게 아니라 찾을 수 있는 디펜더시가 생성됨
+    //private final ObjectProvider<MyLogger> myLoggerProvider; //myLogger 을 주입받는게 아니라 찾을 수 있는 디펜더시가 생성됨
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) { //고객 요청 정보를 받을 수 있음
+        String requestURL = request.getRequestURL().toString(); //어떤 URL 로 들어왔는지 알 수 있음
+        //MyLogger myLogger = myLoggerProvider.getObject(); //필요한 시점에 myLogger 을 주입받음
 
-        String requestURL = request.getRequestURL().toString(); //어떤 URL로 들어왔는지 알 수 있음
-        MyLogger myLogger = myLoggerProvider.getObject(); //필요한 시점에 myLogger을 주입받음음
-        myLogger.setMyLogger(requestURL);
+        System.out.println("myLogger = " + myLogger.getClass());
+        myLogger.setRequestURL(requestURL);
+        System.out.println("myLogger = " + myLogger.getClass());
 
         myLogger.log("controller test");
         logDemoService.logic("testID");
