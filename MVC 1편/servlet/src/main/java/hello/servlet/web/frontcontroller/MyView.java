44bf733393e177db8813f485755160613d2f8b0d
Gyuri Kim <kgr3819@gmail.com>
2022-08-31 00:05:06 +0900

    [mvc1]view 분리-v2

---
+++ MVC 1편/servlet/src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -0,0 +1,22 @@
+package hello.servlet.web.frontcontroller;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MyView {
+
+    private String viewPath;
+
+    public MyView(String viewPath) {
+        this.viewPath = viewPath;
+    }
+
+    //view 를 만드는 행위 자체를 '렌더링한다'
+    public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response); //jsp 가 자동으로 렌더링 됨
+    }
+}
