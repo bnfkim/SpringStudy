a706d6e04c142b4a9bf24fc40d6eb8b4d8f76bc1
Gyuri Kim <kgr3819@gmail.com>
2022-08-31 14:25:23 +0900

    [mvc1] 모델추가-v3

--- MVC 1편/servlet/src/main/java/hello/servlet/web/frontcontroller/v3/FrontControllerServletV2.java
+++ MVC 1편/servlet/src/main/java/hello/servlet/web/frontcontroller/v3/FrontControllerServletV2.java
@@ -1,10 +1,10 @@
-package hello.servlet.web.frontcontroller.v2;
+package hello.servlet.web.frontcontroller.v3;
 
+import hello.servlet.web.frontcontroller.ModelView;
 import hello.servlet.web.frontcontroller.MyView;
-import hello.servlet.web.frontcontroller.v1.ControllerV1;
-import hello.servlet.web.frontcontroller.v2.controller.MemberFormControllerV2;
-import hello.servlet.web.frontcontroller.v2.controller.MemberListControllerV2;
-import hello.servlet.web.frontcontroller.v2.controller.MemberSaveControllerV2;
+import hello.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,15 +15,15 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-@WebServlet(name = "frontControllerServletV2", urlPatterns = "/front-controller/v2/*") //디렉토리 별표 표시-> 어떤 파일이 와도 우선 이 서블릿 우선 실행
-public class FrontControllerServletV2 extends HttpServlet {
+@WebServlet(name = "frontControllerServletV3", urlPatterns = "/front-controller/v3/*") //디렉토리 별표 표시-> 어떤 파일이 와도 우선 이 서블릿 우선 실행
+public class FrontControllerServletV3 extends HttpServlet {
 
-    private Map<String, ControllerV2> controllerMap = new HashMap<>();
+    private Map<String, ControllerV3> controllerMap = new HashMap<>();
 
-    public FrontControllerServletV2() {
-        controllerMap.put("/front-controller/v2/members/new-form", new MemberFormControllerV2());
-        controllerMap.put("/front-controller/v2/members/save", new MemberSaveControllerV2());
-        controllerMap.put("/front-controller/v2/members", new MemberListControllerV2());
+    public FrontControllerServletV3() {
+        controllerMap.put("/front-controller/v3/members/new-form", new MemberFormControllerV3());
+        controllerMap.put("/front-controller/v3/members/save", new MemberSaveControllerV3());
+        controllerMap.put("/front-controller/V3/members", new MemberListControllerV3());
     }
 
     @Override
@@ -31,13 +31,27 @@ public class FrontControllerServletV2 extends HttpServlet {
 
         String requestURI = request.getRequestURI();
 
-        ControllerV2 controller = controllerMap.get(requestURI);
+        ControllerV3 controller = controllerMap.get(requestURI);
         if(controller == null){
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
             return;
         }
-        //ex. 생성결과 : new MyView("/WEB-INF/views/new-form.jsp"); 가 생성되고, render 메서드를 통해서 웹브라우저에 내려가게 됨.
-        MyView view = controller.process(request, response);
+
+        Map<String, String> paramMap = createParamMap(request);
+
+        ModelView mv = controller.process(paramMap);
         view.render(request, response);
+    }
+
+    private Map<String, String> createParamMap(HttpServletRequest request) {
+        //paramMap
+        Map<String, String> paramMap = new HashMap<>();
+
+        //(1)request.getParameterNames -> request의 모든 파라메터 이름을 가져옴
+        //(2)forEachRemaining -> paramName 변수로 하나씩 꺼내옴
+        //(3)paramMap -> 꺼내온 변수를 key 값으로해서 value 를 꺼내옴
+        request.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> paramMap.put(paramName, request.getParameter(paramName)));
+        return paramMap;
     }
 }
