fa5ebe0c440264500823292491e1e06f207ee616
Gyuri Kim <kgr3819@gmail.com>
2022-09-05 19:12:52 +0900

    [mvc1] 핸들러 매핑과 핸들러 어댑터 + 뷰 리졸버

--- MVC 1편/servlet/src/main/java/hello/servlet/ServletApplication.java
+++ MVC 1편/servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -3,6 +3,9 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @ServletComponentScan //서블릿 자동 등록
 @SpringBootApplication
@@ -10,6 +13,11 @@ public class ServletApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
+	}
+
+	@Bean //스프링 내에서 작동되는 방법
+	InternalResourceViewResolver internalResourceViewResolver() {
+		return new InternalResourceViewResolver("/WEB-INF/views/", ".jsp");
 	}
 
 }
