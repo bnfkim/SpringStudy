cd2eaecb2268328214c63ed966a5a60a3bd88e9e
Gyuri Kim <kgr3819@gmail.com>
2022-09-06 12:06:08 +0900

    [mvc1] 스프링MVC - 시작하기

--- MVC 1편/servlet/src/main/java/hello/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
+++ MVC 1편/servlet/src/main/java/hello/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+//(1) @Component 가 붙어있어서 스캔 인식 대상이 되어 빈으로 자동 등록
+//(2) @RequestMapping 과 함께 매핑된다.
+// @Component + @RequestMapping 을 클래스레벨에서 함께 적어두어도 된다.
 public class SpringMemberFormControllerV1 {
 
     @RequestMapping("/springmvc/v1/members/new-form")
