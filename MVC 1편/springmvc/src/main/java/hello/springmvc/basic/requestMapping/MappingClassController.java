ca9cf184d4fd975f14845af5e3e6d76ae4f1a801
Gyuri Kim <kgr3819@gmail.com>
2022-09-08 16:57:11 +0900

    [mvc1] 요청 매핑 - API 예시

--- MVC 1편/springmvc/src/main/java/hello/springmvc/basic/requestMapping/MappingClassController.java
+++ MVC 1편/springmvc/src/main/java/hello/springmvc/basic/requestMapping/MappingClassController.java
@@ -3,6 +3,7 @@ package hello.springmvc.basic.requestMapping;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("mapping/users")
 public class MappingClassController {
 
     /**
@@ -14,27 +15,27 @@ public class MappingClassController {
      * 회원 삭제:       DELETE /users/{userId}
      */
 
-    @GetMapping("/mapping/users")
+    @GetMapping
     public String user() {
         return "get users";
     }
 
-    @PostMapping("/mapping/users")
+    @PostMapping
     public String addUser() {
         return "post user";
     }
 
-    @GetMapping("/mapping/users/{userId}")
+    @GetMapping("/{userId}")
     public String findUser(@PathVariable String userId) {
         return "get userId=" + userId;
     }
 
-    @PatchMapping("/mapping/users/{userId}")
+    @PatchMapping("/{userId}")
     public String updateUser(@PathVariable String userId) {
         return "update userId=" + userId;
     }
 
-    @DeleteMapping("/mapping/users/{userId}")
+    @DeleteMapping("/{userId}")
     public String deleteUser(@PathVariable String userId) {
         return "delete userId=" + userId;
     }
