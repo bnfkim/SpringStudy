f65422382b9f4988c6d9821a893c7e9c206fb6ed
Gyuri Kim <kgr3819@gmail.com>
2022-09-22 16:18:59 +0900

    [mvc1] PRG Post/Redirect/Get

--- MVC 1편/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ MVC 1편/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -31,7 +31,7 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    @GetMapping("/add")
+//    @GetMapping("/add")
     public String addForm(){
         return "basic/addForm";
     }
@@ -63,7 +63,7 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV3(@ModelAttribute Item item){
         itemRepository.save(item);
         return "basic/item";
@@ -75,6 +75,18 @@ public class BasicItemController {
         return "basic/item";
     }
 
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model){
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item){
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
+    }
 
     /**
      * 테스트용 데이터 추가
